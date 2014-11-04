60b5a4ed417825558cb3488cbdaa4a5084aa836b
Arnaud Janssens <aj@zephyr-web.fr>
2014-11-04 09:46:38 +0100

    Updated deploy to "production" and branch master

--- app/config/deploy/prod.rb
+++ app/config/deploy/prod.rb
@@ -1,9 +1,9 @@
 # General settings
 set :domain, "srv01.we-spot-it.com"
-set :deploy_to, "/var/www/test-bo.ardemispartners.com/web"
+set :deploy_to, "/var/www/intranet.ardemispartners.com/web"
 set :user, "root"
 set :repository, "git@bitbucket.org:ardemis/profil_management.git"
-set :branch, "develop"
+set :branch, "master"
 set :clear_controllers, false
 
 server domain, :app, :web, :primary => true
@@ -11,7 +11,7 @@ server domain, :app, :web, :primary => true
 # Composer settings
 set :composer_options, "--no-progress --verbose --optimize-autoloader"
 
-task :ask_testbo_confirmation do
+task :ask_prod_confirmation do
     set(:confirmed) do
         puts <<-WARN
             WARN
@@ -25,4 +25,4 @@ task :ask_testbo_confirmation do
     end
 end
 
-before 'deploy', :ask_testbo_confirmation
+before 'deploy', :ask_prod_confirmation
